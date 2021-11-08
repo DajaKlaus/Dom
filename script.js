3b8f52a27d6abb83bb93a9fc675120cad9152925
DajaKlaus <daja.klaus@iisgalvanimi.edu.it>
2021-11-08 18:04:58 +0000

    dom 1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function btnMostraFunction() {
+    document.getElementById("mytext").innerHTML = "A me piace java script";
+}
+function btnAltroFunction() {
+  document.getElementById("mytext2.0").innerHTML = "A me non piace java script";
+}
